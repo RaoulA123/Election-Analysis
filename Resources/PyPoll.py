97575251fe1062bbb6bd9abeae7d7f460421421d
RaoulA123 <115424156+RaoulA123@users.noreply.github.com>
2023-03-11 18:17:01 -0500

    Add files via upload

---
+++ Resources/PyPoll.py
@@ -0,0 +1,6 @@
+# 1. The data we need to retrieve.
+# 2. The Total number of votes cast
+# 3. The total number of candidates who received votes.
+# 4. The percentage of votes each candidates won
+# 5. The total number of of votes each candidate won
+# 6. The winner of the election based on popular vote.   
